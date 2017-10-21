fa9ecdd6dd188247af34e8d8ec267b4ac640a887
Huy Ngo <huybngo@yahoo.com>
2017-10-20 19:51:22 -0700

    Added slime sprite and nav mesh agent component to it.

---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour, IMovement
+{
+	public Transform playerTransform;
+	private NavMeshAgent m_navMeshAgent;
+
+	private void Awake()
+	{
+		m_navMeshAgent = GetComponent<NavMeshAgent> ();
+	}
+
+	public void Update()
+	{
+		Move (0f);
+	}
+
+	public void Move(float velocityX)
+	{
+		m_navMeshAgent.SetDestination (playerTransform.position);
+	}
+
+	public void Jump(bool jumpFlag)
+	{
+	}
+}
