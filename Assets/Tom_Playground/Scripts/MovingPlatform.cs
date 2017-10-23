7f3123cb6b81d40bc969ac5393e0484b10c91e96
Thomas Halstead <halsteat@uci.edu>
2017-10-23 03:47:29 -0700

    Laid groundwork for player character management, 2D physics on 3D models, and moving platforms.
    Created a PlayerManager script which can automatically load all PlayerController children from
    the scene as well as manage swapping between them and providing them player input.
    
    Created prefabs for a player characters to easily set them up while allowing shape to be added
    afterward. This sets up some default physics settings, tag, and ground detection (to be updated
    later to more accurate ground only detection, as this will detect walls as well. This should
    also be tested with edge colliders instead of box colliders.
    
    Set up moving platforms. Their X,Y minimum and maximum offset from start position can be set,
    as well as X,Y velocity. No hard enforcement of boundaries, so high velocities or those which
    aren't multiples of the offset may cause issues if used.

---
+++ Assets/Tom_Playground/Scripts/MovingPlatform.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour {
+	public Vector2 min_offset = new Vector2 (0, 0);
+	public Vector2 max_offset = new Vector2 (0, 0);
+	public Vector2 speed = new Vector2(1,1);
+
+	private Vector2 min, max;
+	private Rigidbody2D rb2D;
+	// Use this for initialization
+	void Start () 
+	{
+		rb2D = transform.GetComponent<Rigidbody2D> ();
+		min = (Vector2)transform.position - min_offset;
+		max = (Vector2)transform.position + max_offset;
+		if (min_offset != Vector2.zero || max_offset != Vector2.zero) 
+		{
+			rb2D.velocity = speed;
+		}
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		Vector2 set_velocity = rb2D.velocity;
+		if (transform.position.x > max.x || transform.position.x < min.x) 
+		{
+			set_velocity.x = -set_velocity.x;
+		}
+		if (transform.position.y > max.y || transform.position.y < min.y) 
+		{
+			set_velocity.y = -set_velocity.y;
+		}
+		rb2D.velocity = set_velocity;
+	}
+}
---
+++ Assets/Tom_Playground/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	private bool is_grounded = true;
+	public float jump_force = 100.0f;
+	public float move_force = 10.0f;
+	void Start()
+	{
+		
+	}
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		Debug.Log("Collision with: " + other.gameObject.name + ", tag=" + other.gameObject.tag);
+		if (other.gameObject.tag == "Platform") 
+		{
+			is_grounded = true;
+			Debug.Log ("Entered Ground");
+		}
+
+	}
+	void OnCollisionExit2D(Collision2D other)
+	{
+		if (other.gameObject.tag == "Platform") 
+		{
+			is_grounded = false;
+			Debug.Log ("Left ground");
+		}
+	}
+	public bool isGrounded()
+	{
+		return is_grounded;
+	}
+}
