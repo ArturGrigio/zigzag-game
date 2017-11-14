f01988cf2c955ce8b2f85fbfeddce20b4840bab3
Huy Ngo <huybngo@yahoo.com>
2017-11-14 15:19:15 -0800

    Created Assets branch
    - Imported some background assets from Unity Store
    
    - Take a look at Asset/Scenes/Main
    
    - Created a greyscale shader script to turn sprites into black and white
    
    - Added a LockTranslation class to lock the background from moving in the y direction
    
    - Added a file to contain a list of Assets sources

---
+++ Assets/Scripts/Management/LockTranslation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LockTranslation : MonoBehaviour
+{
+	public float LockPositionX;
+	public float LockPositionY;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () 
+	{
+		transform.position = new Vector3 (transform.position.x, LockPositionY, transform.position.z);
+	}
+}
